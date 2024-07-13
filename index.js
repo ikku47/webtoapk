b697af859c60d9dd6afdcde9ab3378003f24637f
IKKU <iqbalsuperfuture@gmail.com>
2024-07-13 10:07:30 +0400

    fixes

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const port = 3000;
 app.use(express.static("public"));
 // app.use(express.json({limit: '50mb'}));
 // app.use(express.urlencoded({ extended: true,limit: '50mb' }));
+fs.ensureDirSync(path.join(__dirname, "uploads"));
+fs.ensureDirSync(path.join(__dirname, "builds"));
+fs.ensureDirSync(path.join(__dirname, "projects"));
 
 const storage = multer.diskStorage({ // notice you are calling the multer.diskStorage() method here, not multer()
     destination: function(req, file, cb) {
@@ -29,9 +32,15 @@ app.post(
     { name: "splashScreen", maxCount: 1 },
   ]),
   async (req, res) => {
-    console.log(req.body);
-    console.log(req.files);
     const { websiteUrl, appName, primaryColor, secondaryColor } = req.body;
+    if (fs.existsSync(path.join(__dirname, 'builds', `${appName}.apk`))){
+      const apkPath = path.join(
+        __dirname,
+        "builds",
+        `${appName.replace(/\s+/g, "_").toLowerCase()}.apk`
+      );
+      return res.json({ success: true, apkPath });
+    }
 
     if (!websiteUrl || !appName) {
       return res.status(400).json({ error: "Missing required parameters" });
@@ -46,8 +55,12 @@ app.post(
         req.files.appLogo[0],
         req.files.splashScreen[0],
       );
+      fs.remove(path.join(__dirname, 'projects', appName));
+      fs.remove(path.join(__dirname, req.files.appLogo[0].path));
+      fs.remove(path.join(__dirname, req.files.splashScreen[0].path));
       res.json({ success: true, apkPath });
     } catch (error) {
+      fs.remove(path.join(__dirname, 'projects', appName));
       res.status(500).json({ error: "Failed to build APK" });
     }
   }
@@ -59,11 +72,7 @@ app.get("/download-apk/:filename", (req, res) => {
 
   if (fs.existsSync(filePath)) {
     res.download(filePath);
-    fs.remove(path.join(__dirname, 'projects', filename.replace('.apk', '')));
   } else {
-    if (fs.existsSync(filePath)){
-      fs.remove(path.join(__dirname, 'projects', filename.replace('.apk', '')));
-    }
     res.status(404).json({ error: "APK file not found" });
   }
 });
